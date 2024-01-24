ebf187d26c3499073b8274f941448f93f1ea6fcb
Christian <mail@christianhohenbild.de>
2024-01-24 17:13:04 +0100

    add CameraControls

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react"
 import { Canvas, useFrame } from '@react-three/fiber'
-import { Environment, OrbitControls, SoftShadows } from '@react-three/drei'
-import { useControls, Leva } from 'leva'
+import { Environment, CameraControls, SoftShadows } from '@react-three/drei'
+import { useControls, Leva, buttonGroup, button } from 'leva'
 
 import './index.css'
 
@@ -10,9 +10,6 @@ import AddText from './AddText.jsx'
 import AddFloor from './AddFloor.jsx'
 import TextIn3D from './TextIn3D.jsx'
 import TroikaText from './TroikaText.jsx'
-
-
-
 
 function LightAnimation({config}){
 
@@ -38,13 +35,37 @@ function LightAnimation({config}){
 
 function App() {
 
+const cameraControlsRef = useRef()
+
   const config = useControls('What is this', {
     backgroundColor : '#c1efef',
     lightIntensity : { value: 1, min: 0.0001, max: 14, step: 0.00001 },
     softShadowSize : { value: 25, min: 0.0, max: 100.0, step: 1.0 },
     softShadowSamples : { value: 10, min: 1, max: 20, step: 1 },
     softShadowFocus : { value: 0, min: 0, max: 2, step: 0.01 },
-    floorSize : { value: 10, min: 1, max: 20, step: 1 }
+    floorSize : { value: 10, min: 1, max: 20, step: 1 },
+    camRotX : { value: 0, min: 0, max: 2*Math.PI, step: 0.1 },
+    camRotY : { value: 0, min: 0, max: 2*Math.PI, step: 0.1 },
+    camRotZ : { value: 0, min: 0, max: 2*Math.PI, step: 0.1 },
+
+    thetaGrp: buttonGroup({
+      label: 'rotate theta',
+      opts: {
+        '+45º': () => cameraControlsRef.current?.rotate(45 * DEG2RAD, 0, true),
+        '-90º': () => cameraControlsRef.current?.rotate(-90 * DEG2RAD, 0, true),
+        '+360º': () => cameraControlsRef.current?.rotate(360 * DEG2RAD, 0, true)
+      }
+    }),
+    phiGrp: buttonGroup({
+      label: 'rotate phi',
+      opts: {
+        '+20º': () => cameraControlsRef.current?.rotate(0, 20 * DEG2RAD, true),
+        '-40º': () => cameraControlsRef.current?.rotate(0, -40 * DEG2RAD, true)
+      }
+    }),
+    saveState: button(() => cameraControlsRef.current?.saveState()),
+    reset: button(() => cameraControlsRef.current?.reset(true)),
+    enabled: { value: true, label: 'controls on' }
   })
 
   return (
@@ -53,12 +74,15 @@ function App() {
     <Canvas 
     shadows
     camera={{ 
-    position: [0, 0, -5], 
-    rotation: [0, 0, 0],
+     
+
     fov: 40 }}    
     >
       
-    <OrbitControls />
+    <CameraControls 
+      ref={cameraControlsRef}
+    />
+
     <SoftShadows 
       size= {config.softShadowSize}
       focus= {config.softShadowFocus}
@@ -68,7 +92,7 @@ function App() {
       
     <color attach="background" args={[config.backgroundColor]} />
       
-    // <LightAnimation config={config}/>
+    <LightAnimation config={config}/>
 
     <AddFloor config={config}/>
       
